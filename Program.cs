11d4ec4dfc23baf9546f644f32f0a3eaf23655c5
Farhan Younas <farhanyounas5204@gmail.com>
2021-12-30 17:51:33 +0500

    First

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=3 , b=4;
+            //CONDITION 1
+            int c = ++a;
+            c *= b++;
+            Console.WriteLine(c); 
+
+            //CONDITION 2
+            int c= ++a;
+            c*= ++b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
